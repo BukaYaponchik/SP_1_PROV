7871b8b1c649e1c09b0543bae65dd8f942dd5ab5
BukaYaponchik <bukovtsov03@gmail.com>
2024-09-11 20:50:10 +0300

    Homework with 4 task (massiv №2)

--- src/Main.java
+++ src/Main.java
@@ -4,51 +4,65 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Задание №1");
 
-        int [] firstMas = new int []{1, 2, 3};
-        double [] secondMas = {1.57, 7.654, 9.986};
-        boolean [] thirdMas = {true, true, true};
+        int [] costDataMonth = {100000, 345800, 23060, 56077, 6500};
+        int sumCostMonth = 0;
+        for (int costDatum : costDataMonth) {
+            sumCostMonth += costDatum;
+        }
+        System.out.println("Сумма трат за месяц составила " + sumCostMonth + " рублей");
 
         System.out.println("Задание №2");
 
-        for (int i = 0; i < firstMas.length; i++) {
-            if(i == firstMas.length - 1) System.out.print(firstMas[i]);
-            else System.out.print(firstMas[i] + ", ");
+        int [] costDataWeek = {23060, 56077, 6500, 100000, 345800};
+        int minCostWeek = 2147483647;
+        int maxCostWeek = -1;
+        for (int miniCostDataWeek : costDataWeek) {
+            if (minCostWeek > miniCostDataWeek) {
+                minCostWeek = miniCostDataWeek;
+            }
+            if (maxCostWeek < miniCostDataWeek) {
+                maxCostWeek = miniCostDataWeek;
+            }
         }
-        System.out.println();
-        for (int i = 0; i < secondMas.length; i++) {
-            if(i == secondMas.length - 1) System.out.print(secondMas[i]);
-            else System.out.print(secondMas[i] + ", ");
-        }
-        System.out.println();
-        for (int i = 0; i < thirdMas.length; i++) {
-            if(i == thirdMas.length - 1) System.out.print(thirdMas[i]);
-            else System.out.print(thirdMas[i] + ", ");
-        }
-        System.out.println();
+        System.out.println("Минимальная сумма трат за неделю составила " + minCostWeek + " рублей. Максимальная сумма трат за неделю составила " + maxCostWeek + " рублей");
 
         System.out.println("Задание №3");
 
-        for (int i = firstMas.length - 1; i >= 0; i--) {
-            if(i == 0) System.out.print(firstMas[i]);
-            else System.out.print(firstMas[i] + ", ");
+        int [] costDataMonth2 = {56077, 345800, 123060, 256077, 6500};
+        float averageCostDataMonth = 0f;
+        for (int i : costDataMonth2) {
+            averageCostDataMonth += i;
         }
-        System.out.println();
-        for (int i = secondMas.length - 1; i >= 0; i--) {
-            if(i == 0) System.out.print(secondMas[i]);
-            else System.out.print(secondMas[i] + ", ");
-        }
-        System.out.println();
-        for (int i = thirdMas.length - 1; i >= 0; i--) {
-            if(i == 0) System.out.print(thirdMas[i]);
-            else System.out.print(thirdMas[i] + ", ");
-        }
-        System.out.println();
+        averageCostDataMonth /= 4;
+        System.out.println("Средняя сумма трат за месяц составила " + averageCostDataMonth + " рублей");
 
         System.out.println("Задание №4");
 
-        for (int i = 0; i < firstMas.length; i++) {
-            if (firstMas[i] % 2 == 1) firstMas[i]++;
+        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        int indexWhitespace = 0;
+        char temporaryVariable;
+        for (int i = 0; i < reverseFullName.length; i++) {
+            if (reverseFullName[i] == ' '){
+                indexWhitespace = i;
+            }
         }
-        System.out.println(Arrays.toString(firstMas));
+
+        int nameLength = indexWhitespace;
+        if (nameLength % 2 == 1) nameLength++;
+        for (int i = 0; i < (nameLength/2); i++) {
+            temporaryVariable = reverseFullName[i];
+            reverseFullName[i] = reverseFullName[nameLength - 1 - i];
+            reverseFullName[nameLength - 1 - i] = temporaryVariable;
+        }
+
+        int variableLength = (reverseFullName.length) - indexWhitespace - 1;
+        if (variableLength % 2 == 1) variableLength++;
+        for (int i = indexWhitespace + 1; i < indexWhitespace + (variableLength/2) + 1; i++) {
+            temporaryVariable = reverseFullName[i];
+            reverseFullName[i] = reverseFullName[reverseFullName.length + indexWhitespace - i];
+            reverseFullName[reverseFullName.length + indexWhitespace - i] = temporaryVariable;
+        }
+
+        System.out.println(Arrays.toString(reverseFullName));
     }
 }
