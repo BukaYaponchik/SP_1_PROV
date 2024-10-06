595c37b9430707868e310bdcd6b683e99026f204
BukaYaponchik <bukovtsov03@gmail.com>
2024-10-06 17:43:28 +0300

    Session is invalid part 1

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,80 @@
+import java.util.Random;
+
 public class Main {
 
+    private final static Random RANDOM = new Random();
+    private final static String[] NAMES = {"Иван", "Петр", "Сергей", "Владимир", "Максим", "Андрей", "Олег"};
+    private final static String[] SURNAMES = {"Иванов", "Петров", "Сергеев", "Владимиров", "Максимов", "Андреев", "Олегов"};
+    private final static String[] PATRONYMIC = {"Иванович", "Петрович", "Сергеевич", "Владимирович", "Максимович", "Андреевич", "Олегович"};
+
+   private static Employee[] EMPLOYEES = new Employee[10];
+
+   private static void initEmployees(){
+       for (int i = 0; i < EMPLOYEES.length; i++) {
+           String fullName = SURNAMES[RANDOM.nextInt(0, SURNAMES.length)] + " " +
+                   NAMES[RANDOM.nextInt(0, NAMES.length)] + " " +
+                   PATRONYMIC[RANDOM.nextInt(0, PATRONYMIC.length)];
+           EMPLOYEES[i] = new Employee(fullName, RANDOM.nextInt(0, 6), RANDOM.nextInt(50_000, 100_000));
+       }
+   }
+
+   public static void outputOfAllElements(Employee[] employees){
+        for (Employee employee : employees) {
+            System.out.println(employee.toString());
+        }
+    }
+
+   public static int totalSalary(Employee[] employees){
+       int summa = 0;
+       for (Employee employee : employees) {
+           summa += employee.getSalary();
+       }
+       return summa;
+   }
+
+   public static Employee minSalaryEmployee(Employee[] employees){
+       Employee minEmployee = null;
+       for (Employee employee : employees) {
+           if (minEmployee == null || minEmployee.getSalary() < employee.getSalary()) {
+               minEmployee = employee;
+           }
+       }
+       return minEmployee;
+   }
+
+    public static Employee maxSalaryEmployee(Employee[] employees){
+        Employee maxEmployee = null;
+        for (Employee employee : employees) {
+            if (maxEmployee == null || maxEmployee.getSalary() > employee.getSalary()) {
+                maxEmployee = employee;
+            }
+        }
+        return maxEmployee;
+    }
+
+    public static double averageSalary(Employee[] employees){
+       return (double) totalSalary(employees) / employees.length;
+    }
+
+    public static void fullNameAllEmployee(Employee[] employees){
+        for (Employee employee : employees) {
+            System.out.println(employee.getFullName());
+        }
+    }
+
     public static void main(String[] args) {
-        Author pushkin = new Author("Александр", "Пушкин");
-        Author vysotsky = new Author("Владимир", "Высоцкий");
-
-        Book dubrovsky = new Book("Дубровкий", pushkin, 1840);
-        Book collectionVysotsky = new Book("Сборник произведений Высоцкого", vysotsky, 1983);
-
-        System.out.println("Использование toString с форматным выводом");
-        System.out.println("----Первая книга----");
-        System.out.println(dubrovsky.toString());
-        System.out.println("----Вторая книга----");
-        System.out.println(collectionVysotsky.toString());
-        System.out.println("----Первый автор----");
-        System.out.println(pushkin.toString());
-        System.out.println("----Второй автор----");
-        System.out.println(vysotsky.toString());
-        System.out.println();
-
-        System.out.println("Использование equals, где для книг не сравнивается год выпуска");
-        System.out.println("Проверка Дубровского и Сборника ->" + dubrovsky.equals(collectionVysotsky));
-        Book dubrovsky2 = new Book("Дубровкий", pushkin, 1840);
-        System.out.println("Проверка Дубровского и Дубровского2 ->" + dubrovsky.equals(dubrovsky2));
-        dubrovsky2.setYearPublication(1841);
-        System.out.println("Проверка Дубровского и Дубровского2 с разными годами выпуска ->" + dubrovsky.equals(dubrovsky2));
-        System.out.println();
-
-        System.out.println("Использование hashCode");
-        System.out.println("Проверка hash Дубровского и Сборника ->" + dubrovsky.hashCode() + " " + collectionVysotsky.hashCode());
-        System.out.println("Проверка hash Дубровского и Дубровского2 ->" + dubrovsky.hashCode() + " " + dubrovsky2.hashCode());
-
+        initEmployees();
+        System.out.println("Список сотрудников");
+        outputOfAllElements(EMPLOYEES);
+        System.out.println("Общая ЗП за месяц");
+        System.out.println(totalSalary(EMPLOYEES));
+        System.out.println("Минимальная ЗП");
+        System.out.println(minSalaryEmployee(EMPLOYEES).getSalary());
+        System.out.println("Максимальная ЗП");
+        System.out.println(maxSalaryEmployee(EMPLOYEES).getSalary());
+        System.out.println("Средняя ЗП");
+        System.out.println(averageSalary(EMPLOYEES));
+        System.out.println("ФИО всех сотрудников");
+        fullNameAllEmployee(EMPLOYEES);
     }
 }
